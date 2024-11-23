ade2f78c8e0d33e16a03363440092485458c3ff1
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-11-23 11:02:07 +1100

    Creation of SearchEvents page
    Created SearchEvents page and css styling of the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-<script src="https://kit.fontawesome.com/238c66733a.js" crossorigin="anonymous"></script>
+import SearchEvents from "./components/SearchEvents";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<h1>Sign in Page</h1>} />
-        <Route path="/events" element={<h1>New Event Page</h1>} />
-        <Route path="/create" element={<h1>Create Event Page</h1>} />
-        <Route path="/myevents" element={<h1>My Events</h1>} />
-        <Route path="/searchevents" element={<h1>Search Events</h1>} />
-        <Route path="/calendar" element={<h1>My Calendar</h1>} />
-        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route exact path="/" element={<h1>Sign in Page</h1>} />
+        <Route exact path="/events" element={<h1>New Event Page</h1>} />
+        <Route exact path="/create" element={<h1>Create Event Page</h1>} />
+        <Route exact path="/myevents" element={<h1>My Events</h1>} />
+        <Route exact path="/searchevents" element={<SearchEvents />} />
+        <Route exact path="/calendar" element={<h1>My Calendar</h1>} />
+        <Route exact path="/profile" element={<h1>Profile Page</h1>} />
       </Routes>
     </div>
   );
