74e76c84cf571d50738b3e0319dc08d78852ee35
hsc996 <han.scaife@gmail.com>
2024-12-16 17:06:58 +1100

    feat: login page displaying error messages to user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,81 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "../css/global.css";
 import "../css/Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    setErrorMessage("");
+
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/account/signin`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
+      });
+
+      const data = await response.json();
+
+      if (response.ok){
+        localStorage.setItem("User", JSON.stringify(data.user));
+        alert("Login successful!");
+        setErrorMessage("");
+      } else {
+        setErrorMessage(data.message || "Something went wrong, please try again.");
+      }
+    } catch (error) {
+      console.error("An error occurred during login: ", error);
+      setErrorMessage("An error occurred while trying to login. Please try again later")
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     
     <div className="login-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleLogin}>
         <h1>Login</h1>
 
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" placeholder="Username" />
-
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="example@email.com" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="example@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="******" />
-        <button type="submit" className="button login-button">
-          LOGIN
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
+        {/* Display error message if it exists */}
+        {errorMessage && (
+          <div className="error-messages">
+            <div className="error-message">{errorMessage}</div>
+          </div>
+        )}
+        <button type="submit" className="button login-button" disabled={loading}>
+        {loading ? "Logging in..." : "LOGIN"}
         </button>
       </form>
     </div>
